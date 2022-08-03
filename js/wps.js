5cb0c11995c3861faa6107572e030d4950c2e783
liyingjie <lyj0116@gmail.com>
2022-08-03 17:09:06 +0800

    Update wps.js

--- js/wps.js
+++ js/wps.js
@@ -4,45 +4,38 @@
 ^https:\/\/account\.wps\.cn\/api\/users url script-response-body https://raw.githubusercontent.com/ackakakaka/QuantumultX/master/wps.js
 
 [MITM]
-hostname = account.wps.cn
+hostname = *account.wps.cn, *account.wps.com
 */
 
-var obj = JSON.parse($response.body);
-
-var id = obj.userid;
-
-obj = {
+var body = JSON.parse($response.body);
+var obj = {
   exp: 0,
   level: 3,
   privilege: [
-    { spid: "data_recover", times: 0, expire_time: 1703952000 },
-    { spid: "ocr", times: 0, expire_time: 1703952000 },
-    { spid: "pdf2doc", times: 0, expire_time: 1703952000 },
-    { spid: "pdf_merge", times: 0, expire_time: 1703952000 },
-    { spid: "pdf_sign", times: 0, expire_time: 1703952000 },
-    { spid: "pdf_split", times: 0, expire_time: 1703952000 }
+    { spid: "data_recover", times: 0, expire_time: 1846256142 },
+    { spid: "ocr", times: 0, expire_time: 1846256142 },
+    { spid: "pdf2doc", times: 0, expire_time: 1846256142 },
+    { spid: "pdf_merge", times: 0, expire_time: 1846256142 },
+    { spid: "pdf_sign", times: 0, expire_time: 1846256142 },
+    { spid: "pdf_split", times: 0, expire_time: 1846256142 }
   ],
   result: "ok",
   total_buy: 0,
   total_cost: -30,
-  userid: 0000,
+  userid: body.userid,
   vip: {
     name: "超级会员",
     has_ad: 0,
     memberid: 40,
-    expire_time: 1703952000,
+    expire_time: 1846256142,
     enabled: [
-      { memberid: 40, name: "超级会员", expire_time: 1703952000 },
-      { memberid: 20, name: "WPS会员", expire_time: 1703952000 },
-      { memberid: 12, name: "稻壳会员", expire_time: 1703952000 }
+      { memberid: 40, name: "超级会员", expire_time: 1846256142 },
+      { memberid: 20, name: "WPS会员", expire_time: 1846256142 },
+      { memberid: 12, name: "稻壳会员", expire_time: 1846256142 }
     ]
   },
   wealth: 0,
-  expire_time: 1703952000
+  expire_time: 1846256142
 };
 
-obj.userid = id;
-
 $done({ body: JSON.stringify(obj) });
-
-// by eHpo1
